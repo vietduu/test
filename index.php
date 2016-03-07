b10e1804b3f11c8998e34626b6ff50aad0a7941d
vietduu <hungvietduu@gmail.com>
2016-03-07 13:58:58 +0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//1.Write a code to print your name in any language
+echo "My name is Viet Duu" . PHP_EOL;
+
+//2. Write a code to sum up the first 1000 numbers. You can use any language.
+$length = 1000;
+$sum = 0;
+for ($i=1;$i<=$length;$i++){
+	$sum = $sum + $i;
+}
+echo "Sum of 1000 first numbers is " . $sum . PHP_EOL;
+
+//4. Create a website
+
+?>
